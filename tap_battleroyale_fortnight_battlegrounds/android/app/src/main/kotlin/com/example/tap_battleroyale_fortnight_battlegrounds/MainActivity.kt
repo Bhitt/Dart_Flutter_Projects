7d00bdc06f55ee0e03841df41d491cf16ebd3200
bhitt <BrandenHitt@gmail.com>
2019-12-02 13:55:23 -0800

    +added garbage
    Signed-off-by: bhitt <BrandenHitt@gmail.com>

---
+++ tap_battleroyale_fortnight_battlegrounds/android/app/src/main/kotlin/com/example/tap_battleroyale_fortnight_battlegrounds/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tap_battleroyale_fortnight_battlegrounds
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
